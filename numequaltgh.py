efff4e29d78391b5690ca054d56a790dc97a8929
Gilmar Junior <54118408+Gilnior@users.noreply.github.com>
2020-11-11 17:30:58 -0300

    Update numequaltgh.py

--- numequaltgh.py
+++ numequaltgh.py
@@ -1,19 +1,18 @@
 print("=-" * 33)
-print("Verifico quantos números ou letras iguais adjacentes há.")
+print("Verify how many times the same chars meet.")
 print("=-" * 33)
 
 num = input('Manda: ')
 
-
 ax = 0
 n = 1
-encontros = 0
+meetings = 0
 
 while not ax == len(num) - 1:
 	if num[ax] == num[n]:
-		encontros += 1
+		meetings += 1
 	ax += 1
 	if not (len(num) - 1) == n:
 		n += 1
 
-print(f"Foi/Foram detectada (s) {encontros} ocorrência (s).")
+print(f"There are {meetings} meeting (s).")
